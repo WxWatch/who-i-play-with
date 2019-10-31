eb2f660273edc3d7bdcc765bde3757964375db74
amphy <amphy.kit@gmail.com>
2019-10-30 21:16:07 -0500

    Added query parsing for route in Results and changed default results route in Router

--- who-i-play-with/src/Router.js
+++ who-i-play-with/src/Router.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import Results from './Results';
 
 class Router extends Component {
 	render() {
 		return (
-			<Router>
+			<BrowserRouter>
 				<div>
 					<Route exact path='/' component={App} />
-					<Route path='/results?region=:region&summoner=:summonerName' component={Results} />
+					<Route path='/results' component={Results} />
 				</div>
-			</Router>
+			</BrowserRouter>
 			);
 	}
 }
